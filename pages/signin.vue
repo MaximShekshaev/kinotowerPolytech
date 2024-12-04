955d7a0fff0f3467600b16be65eb1767a8e13675
Maxim Shekshaev <maximshekshaev@gmail.com>
2024-12-04 12:46:06 +0600

    feat: add functional

---
+++ pages/signin.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="row mt-4">
+    <div class="col md-4"></div>
+    <div class="col md-4">
+      <div class="card">
+        <div class="card-body text-center">
+          <form @submit.prevent="register" action="">
+            <h1>Authentication</h1>
+            <div>
+              <label for="email" class="form-label">Email address</label>
+              <input v-model="email" type="email" class="form-control" id="email" placeholder="name@example.com">
+            </div>
+            <div>
+              <label for="fio" class="form-label">Name</label>
+              <input v-model="fio" type="text" class="form-control" id="fio" >
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
---
+++ stores/gender.js
@@ -0,0 +1,15 @@
+import {api} from "~/api/index.js";
+
+
+ export const useGenderStore = defineStore('gender', () =>{
+   const genders = ref([]);
+   const fetchGenders = async () => {
+       const res = await  api.get("/genders");
+       genders.value = res.data.genders;
+   }
+   fetchGenders();
+   return{
+       genders,
+   }
+
+});
